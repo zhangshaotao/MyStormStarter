f765b7f60e55d013057bd4f7637b7a19d459bb24
zhangshaotao <zhangshaotao.gg@gmail.com>
2014-11-20 17:20:02 +0800

    init

---
+++ src/main/java/com/baihe/storm/topology/WordCountTopology.java
@@ -0,0 +1,13 @@
+package com.baihe.storm.topology;
+
+public class WordCountTopology {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
